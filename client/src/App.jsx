f5276e0ffc81cb0655de856566a7895a9712933e
Mustafa Yasser Soudi <40180893+Nostafa@users.noreply.github.com>
2022-04-16 14:48:54 +0200

    file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navbar,
+  Services,
+  Transactions,
+  Welcome,
+  Loader,
+  Footer,
+} from './components';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 }
